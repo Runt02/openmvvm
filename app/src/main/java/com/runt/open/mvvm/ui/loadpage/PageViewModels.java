b026ff4ba3ae0719de8755a25a374cc75f059f27
nilupeng <qingingrunt2010@qq.com>
2022-08-09 12:10:45 +0800

    合并优化

--- app/src/main/java/com/runt/open/mvvm/ui/loadpage/PageViewModels.java
+++ app/src/main/java/com/runt/open/mvvm/ui/loadpage/PageViewModels.java
@@ -1,13 +1,13 @@
 package com.runt.open.mvvm.ui.loadpage;
 
 import com.runt.open.mvvm.base.model.LoadPageViewModel;
-import com.runt.open.mvvm.ui.main.home.Message;
+import com.runt.open.mvvm.data.Results;
 
 /**
  * @purpose Created by Runt (qingingrunt2010@qq.com) on 2022/8/9.
  */
 public class PageViewModels {
-    public class HomeViewModel extends LoadPageViewModel<Message> {
+    public class HomeViewModel extends LoadPageViewModel<Results.Message> {
         @Override
         protected String requestUrl() {
             return "getMsgList";
